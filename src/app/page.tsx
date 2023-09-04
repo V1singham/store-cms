1a12fbfa6c9a65ac8d2a9ee4f0edcdfdc2903a78
vikash.singh10 <vikash.singh@pw.live>
2023-09-04 21:59:58 +0530

    wip changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/Button";
 export default function IndexPage() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div>
+      <div className="flex gap-2">
         <Button variant={"outline"}>Button</Button>
         <Button variant={"ghost"}>Button</Button>
         <Button variant={"link"}>Button</Button>
